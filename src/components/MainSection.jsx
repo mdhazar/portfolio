b6e1d149557e814f2baa53c30fb1702eaac6778f
mdhazar <m.derya.hazar@gmail.com>
2024-10-09 16:39:05 +0300

    Adjust images

--- src/components/MainSection.jsx
+++ src/components/MainSection.jsx
@@ -1,42 +1,57 @@
 import { FaGithub, FaLinkedin } from 'react-icons/fa';
 import { useLanguage } from '../contexts/LanguageContext';
 import { useLinks } from '../contexts/LinksContext';
+
 function MainSection() {
-    const { language } = useLanguage();
-    const links = useLinks();
-    return (
-        <main className="px-8 mt-12 mb-12">
-        <div className="flex flex-col lg:flex-row justify-between items-center">
-            
-            <div className="text-left lg:w-1/2">
-            <h2 className="text-4xl font-bold mb-2 dark:text-[#AEBCCF]">{language.welcomeMessage1}<br /> {language.welcomeMessage2}</h2>
+  const { language } = useLanguage();
+  const links = useLinks();
+
+  return (
+    <main className=" mt-12 mb-12">
+      <div className="flex flex-col lg:flex-row justify-between h-[500px]">
+        
+        <div className="text-left lg:w-1/2 h-full flex flex-col justify-between">
+          <div>
+            <h2 className="text-4xl font-bold mb-2 dark:text-[#AEBCCF]">
+              {language.welcomeMessage1}
+              <br />
+              {language.welcomeMessage2}
+            </h2>
             <p className="mb-8 text-gray-600 dark:text-gray-400">
-                {language.introText}
+              {language.introText}
             </p>
-            
-            <div className="flex gap-4">
-            <a href={links.email}> <button className="bg-[#4338CA]      text-white py-3 px-6 rounded-md dark:bg-[#E1E1FF] dark:text-black">
+          </div>
+
+          <div className="flex gap-4">
+            <a href={links.email}>
+              <button className="bg-[#4338CA] text-white py-3 px-6 rounded-md dark:bg-[#E1E1FF] dark:text-black">
                 {language.hireMe}
-                </button></a>
-                <a href={links.github}><button className="border border-[#4338CA] text-[#4338CA] py-3 px-6 rounded-md flex items-center dark:bg-[#383838] dark:text-[#E1E1FF] dark:border-[#E1E1FF]">
+              </button>
+            </a>
+            <a href={links.github}>
+              <button className="border border-[#4338CA] text-[#4338CA] py-3 px-6 rounded-md flex items-center dark:bg-[#383838] dark:text-[#E1E1FF] dark:border-[#E1E1FF]">
                 <FaGithub className="mr-2" /> Github
-                </button> </a>
-                <a href={links.linkedin}><button className="border border-[#4338CA] text-[#4338CA] py-3 px-6 rounded-md flex items-center dark:bg-[#383838] dark:text-[#E1E1FF] dark:border-[#E1E1FF]">
+              </button>
+            </a>
+            <a href={links.linkedin}>
+              <button className="border border-[#4338CA] text-[#4338CA] py-3 px-6 rounded-md flex items-center dark:bg-[#383838] dark:text-[#E1E1FF] dark:border-[#E1E1FF]">
                 <FaLinkedin className="mr-2" /> Linkedin
-                </button></a>
-            </div>
-            </div>
-            
-            <div className="lg:w-1/3 flex justify-center lg:justify-end mt-8 lg:mt-0">
-            <img 
-                src={`https://picsum.photos/500`}  
-                alt="Random"
-                className="w-[500px] h-[500px] object-cover rounded-lg"
-            />
-            </div>
+              </button>
+            </a>
+          </div>
         </div>
-        </main>
-    );
+
+        {/* Image Side */}
+        <div className="lg:w-1/3 flex justify-center lg:justify-end mt-8 lg:mt-0 h-full">
+          <img
+            src={`https://picsum.photos/500`}
+            alt="Random"
+            className="w-full h-full object-cover rounded-lg"
+          />
+        </div>
+      </div>
+    </main>
+  );
 }
 
 export default MainSection;
--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -6,22 +6,24 @@ function ProjectsSection() {
   return (
     <section className="mb-12">
       <section id="projects">
-      {/* Projects content */}
+      
       </section>
       <h3 className="text-2xl font-bold mb-8 dark:text-[#AEBCCF]">{language.projects}</h3>
      
       <div className="grid grid-cols-3 gap-10"> 
         {projects.map((project) => (
           <div key={project} className="bg-white dark:bg-[#252128] rounded-md  text-[#4338CA]  dark:text-[#CFCBFF]"> 
+            <img 
+                src={`https://picsum.photos/300`} className="w-[400px] h-[400px] object-cover rounded-lg  mb-4"></img>
             <h4 className="text-xl font-semibold mb-2">{project}</h4>
+            
             <p className="text-gray-600 dark:text-white mb-4">
               A simple, customizable, minimal setup cookie plugin that allows your users to select which cookies to accept or decline.
             </p>
             
             
             <div className="flex flex-col ">
-            <img 
-                src={`https://picsum.photos/300`} className="w-[400px] h-[400px] object-cover rounded-lg"></img>
+            
               <div className="flex gap-1">
               
                 <div className="border-2 px-3 py-1 text-sm border-[#4338CA] text-[#4338CA]  rounded-md dark:bg-[#383838] dark:text-[#8F88FF]  dark:border-[#8F88FF]">
