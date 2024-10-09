3cea0bef9b35c7a81e472401f7bb4eea891dad70
mdhazar <m.derya.hazar@gmail.com>
2024-10-09 16:34:13 +0300

    Add links

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,13 +1,14 @@
 import { useLanguage } from '../contexts/LanguageContext';
-
+import { useLinks } from '../contexts/LinksContext';
 function Header() {
   const { language } = useLanguage();
+  const links = useLinks();
   return (
     <header className="flex justify-between items-center px-8 py-4">
       <div className="flex items-center gap-16 ml-auto">
-        <button className="bg-transparent border-none text-[#6B7280] no-underline cursor-pointer">{language.skills}</button>
-        <button  className="bg-transparent border-none text-[#6B7280] no-underline cursor-pointer">{language.projects}</button>
-        <button className="border border-[#4338CA] text-[#4338CA] py-3 px-6 rounded-md flex items-center">{language.hireMe}</button>
+        <a href="#skills" className="bg-transparent border-none text-[#6B7280] no-underline cursor-pointer">{language.skills}</a>
+        <a  href="#projects" className="bg-transparent border-none text-[#6B7280] no-underline cursor-pointer">{language.projects}</a>
+        <a href={links.email} className="border border-[#4338CA] text-[#4338CA] py-3 px-6 rounded-md flex items-center">{language.hireMe}</a>
       </div>
     </header>
   );
--- src/components/ProjectsSection.jsx
+++ src/components/ProjectsSection.jsx
@@ -5,6 +5,9 @@ function ProjectsSection() {
   const { language } = useLanguage();
   return (
     <section className="mb-12">
+      <section id="projects">
+      {/* Projects content */}
+      </section>
       <h3 className="text-2xl font-bold mb-8 dark:text-[#AEBCCF]">{language.projects}</h3>
      
       <div className="grid grid-cols-3 gap-10"> 
@@ -17,8 +20,10 @@ function ProjectsSection() {
             
             
             <div className="flex flex-col ">
-              
+            <img 
+                src={`https://picsum.photos/300`} className="w-[400px] h-[400px] object-cover rounded-lg"></img>
               <div className="flex gap-1">
+              
                 <div className="border-2 px-3 py-1 text-sm border-[#4338CA] text-[#4338CA]  rounded-md dark:bg-[#383838] dark:text-[#8F88FF]  dark:border-[#8F88FF]">
                   react 
                 </div>
@@ -30,6 +35,7 @@ function ProjectsSection() {
                 </div>
               </div>
               <div className="flex justify-between gap-4 mt-2">
+              
                 <a href="https://github.com/mdhazar" className="text-[#3730A3] underline ">Github</a>
                 <a href="https://vercel.com/mehmet-s-projects-7ea2b191" className="text-[#3730A3] underline">View Site</a>
               </div>
